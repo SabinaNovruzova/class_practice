6087365fe27ef3feff922889a76ec897f15e246e
SabinaNovruzova <tu7cldm19@code.edu.az>
2022-03-11 02:13:41 +0400

    first commit

---
+++ practice/practice/Model/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practice.Model
+{
+    class Student:Person
+    {
+        public Student()
+        {
+        }
+        public Student(string name)
+        {
+            this.name = name;
+        }
+        public Student (string name, string surname) : this(name)
+        {
+             this.surname = surname;
+        }
+        public Student(string name, string surname, int age):this(name,surname)
+        {
+            this.age = age;
+        }
+    }
+}
---
+++ practice/practice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using practice.Model;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region anonimus class
+            //var telebe1 = new
+            //{
+            //    name = "Sebine",
+            //    surname = "Novruzova",
+            //    age = 21
+            //};
+            //var telebe2 = new
+            //{
+            //    name = "Heyat",
+            //    surname = "Abdullayeva",
+            //    age = 20
+            //};
+            #endregion
+            #region class
+            //Student telebe1 = new Student();
+            //telebe1.name = "Sebine";
+            //telebe1.surname = "Novruzova";
+            //telebe1.FullName();
+            //Student telebe2 = new Student();
+            //telebe2.name = "Nezrin";
+            //telebe2.surname = "Zeynalova";
+            //telebe2.age = 19;
+            //telebe2.GetInfo();
+            Student stu1 = new Student("Sebine", "Novruzova", 21);
+            Console.WriteLine($"Ad:{stu1.name}, Soyad:{stu1.surname}, Yaş:{stu1.age}");
+            Teacher t1 = new Teacher();
+        }
+            #endregion
+        
+        
+           
+        
+    }
+}
+
+
+
